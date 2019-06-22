f03dca0a08cb3b336aaad9711d0d2a9f078e962c
DrZin <61010707@kmitl.ac.th>
2019-06-23 06:54:25 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // move project to github 
+//update again
 import MyApp from "./components/myApp";
 
 ReactDOM.render(<MyApp />, document.getElementById("root"));
