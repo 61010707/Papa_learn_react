9b88fba4084471c110c2cebe132df9bc1fb28322
DrZin <61010707@kmitl.ac.th>
2019-06-23 06:50:08 +0700

    first commit

---
+++ src/components/myApp.js
@@ -0,0 +1,16 @@
+import React from "react";
+function MyApp() {
+  return (
+    <ul>
+      <h1>DrZin</h1>
+      <p>This is react sessions from freecode camp</p>
+      <ul>
+        <li>home</li>
+        <li>Lab</li>
+        <li>My bed</li>
+      </ul>
+    </ul>
+  );
+}
+
+export default MyApp;
