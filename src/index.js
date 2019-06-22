9a33626216acc23881f6bc2955b51f798751069b
DrZin <61010707@kmitl.ac.th>
2019-06-23 06:58:12 +0700

    remove some comment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// move project to github 
-//update again again again
 import MyApp from "./components/myApp";
 
 ReactDOM.render(<MyApp />, document.getElementById("root"));
