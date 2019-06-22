aca027ad26db392b9f61276fe5fba34692d72750
DrZin <61010707@kmitl.ac.th>
2019-06-23 06:53:32 +0700

    move project to github

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
+// move project to github 
 import MyApp from "./components/myApp";
 
 ReactDOM.render(<MyApp />, document.getElementById("root"));
