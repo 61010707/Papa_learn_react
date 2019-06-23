ed864b945b243c5fef6b3163c4978dd366c9e704
DrZin <61010707@kmitl.ac.th>
2019-06-24 01:16:42 +0700

    can fetch data from api

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -1,17 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class myApp extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = {
+      loading: false,
+      character: {}
+    };
   }
 
   componentDidMount() {
-    
+    this.setState({
+      loading: true
+    });
+    fetch("https://swapi.co/api/people/1")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          loading: false,
+          character: data
+        });
+      });
   }
   render() {
+    const text = this.state.loading
+      ? "loading..."
+      : this.state.character.name;
     return (
-      <div>Hello</div>
+      <div>
+        <h1>{text}</h1>
+      </div>
     );
   }
 }
