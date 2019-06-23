e0e194e6e849345b67cd87681f23ecb3a5cdab42
DrZin <61010707@kmitl.ac.th>
2019-06-23 21:44:08 +0700

    reset myapp

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -1,64 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 
-// class MyApp extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isLoggedIN: false
-//     };
-//   }
-
-//   handleClick() {
-//     console.log("i was click");
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <h1>your are current logged {this.state.isLoggedIN ? "in" : "out"}</h1>
-//         <div>
-//           <img
-//             onMouseOver={() => console.log("hover me")}
-//             src="https://fillmurray.com/200/100"
-//             alt="test img"
-//           />
-//           <br />
-//           <br />
-//           <button
-//             onClick={() => {
-//               console.log("i was click");
-//             }}
-//           >
-//             Clike me!!
-//           </button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-class MyApp extends React.Component {
+class MyApp extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0
-    };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {};
   }
 
-  handleClick() {
-    this.setState(prevState => {
-      return {
-        count: prevState.count + 1
-      };
-    });
-  }
   render() {
-    return (
-      <div className="product-list">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>change</button>
-      </div>
-    );
+    return <div>Code goes here</div>;
   }
 }
+
 export default MyApp;
