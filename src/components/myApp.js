f39d4b16c30f531f1a6ccd3f2f58a2f47cb0d16d
DrZin <61010707@kmitl.ac.th>
2019-06-23 20:52:17 +0700

    add event handling and consolr log

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -8,10 +8,29 @@ class MyApp extends React.Component {
     };
   }
 
+  handleClick() {
+    console.log("i was click");
+  }
   render() {
     return (
       <div>
         <h1>your are current logged {this.state.isLoggedIN ? "in" : "out"}</h1>
+        <div>
+          <img
+            onMouseOver={() => console.log("hover me")}
+            src="https://fillmurray.com/200/100"
+            alt="test img"
+          />
+          <br />
+          <br />
+          <button
+            onClick={() => {
+              console.log("i was click");
+            }}
+          >
+            Clike me!!
+          </button>
+        </div>
       </div>
     );
   }
