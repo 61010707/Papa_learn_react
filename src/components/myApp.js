7e5dae41b3264aaf73c18fa0be49178dc1e0d9f0
DrZin <61010707@kmitl.ac.th>
2019-06-23 16:22:00 +0700

    use class base and method

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ProductsData from "./vschoolProducts";
 import ProductList from "./productList";
-const MyApp = () => {
-  const productComponent = ProductsData.map(data => (
-    <ProductList
-      key={data.id}
-      product={data}
-    />
-  ));
-  return <div className="product-list">{productComponent}</div>;
-};
+
+class MyApp extends React.Component {
+  myMethod() {
+    const productComponent = ProductsData.map(data => (
+      <ProductList key={data.id} product={data} />
+    ));
+    return productComponent;
+  }
+  render() {
+    const incomponent = this.myMethod();
+    return <div className="product-list">{incomponent} </div>;
+  }
+}
 export default MyApp;
