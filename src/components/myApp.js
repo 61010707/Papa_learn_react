d2a3fc05a259d3d580cdb04e00d690e10f307bf3
DrZin <61010707@kmitl.ac.th>
2019-06-24 00:27:40 +0700

    react life cyccle

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -6,6 +6,32 @@ class MyApp extends Component {
     this.state = {};
   }
 
+  
+  componentDidMount() {
+    //Get the data I need to correctly display
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // receive props from parent
+    if (nextProps.whateever !== this.props.whateever) {
+      // do some thing
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // return true if want it to update
+    // return false if not
+  }
+
+  componentWillUnmount() {
+    //unmount from screen 
+    // remove or clenup your code your components 
+    //(E.g remove events )
+  }
+  getSnapshotBeforeUpdate()
+  {
+    //create a backup of current way 
+  }
   render() {
     return <div>Code goes here</div>;
   }
