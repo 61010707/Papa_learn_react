b249bbf80f07919d491a4f3e4f0759a91d237fcf
DrZin <61010707@kmitl.ac.th>
2019-06-23 21:12:23 +0700

    add set stste and blind method

---
+++ src/components/toDo.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: props.datas.completed,
+      text: props.datas.text
+    };
+  }
+
+  render() {
+    return (
+      <div className="todo-items">
+        <input
+          type="checkbox"
+          checked={this.state.completed}
+          onChange={() => {
+            console.log("click me !");
+          }}
+        />
+        <p>{this.state.text}</p>
+      </div>
+    );
+  }
+}
+export default ToDo;
