cd59220a823aa80908ecbf6b1acdde7eaaa72d06
DrZin <61010707@kmitl.ac.th>
2019-06-23 09:27:12 +0700

    use props to sent data

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -1,13 +1,39 @@
 import React from "react";
-import Footer from "./footer";
-import NavBar from "./navBar";
-import MainContent from "./mainContent";
-
+import ContactCard from "./contactCard";
 const MyApp = () => (
   <div>
-    <NavBar />
-    <MainContent />
-    <Footer />
+    <ContactCard
+      contact={{
+        name: "Mr. Whiskerson",
+        phone: "(212) 555-1234",
+        email: "mr.whiskaz@catnap.meow",
+        imgUrl: "http://placekitten.com/300/200"
+      }}
+    />
+    <ContactCard
+      contact={{
+        name: "Fluffykins",
+        phone: "(212) 555-2345",
+        email: "fluff@me.com",
+        imgUrl: "http://placekitten.com/400/200"
+      }}
+    />
+    <ContactCard
+      contact={{
+        name: "Destroyer",
+        phone: "(212) 555-3456",
+        email: " ofworlds@yahoo.com",
+        imgUrl: "http://placekitten.com/400/300"
+      }}
+    />
+    <ContactCard
+      contact={{
+        name: "Felix",
+        phone: "(212) 555-4567",
+        email: "thecat@hotmail.com",
+        imgUrl: "http://placekitten.com/200/100"
+      }}
+    />
   </div>
 );
 export default MyApp;
