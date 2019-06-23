2fbb707ffdf8048e25b5a84f265bc36529a0fc7c
DrZin <61010707@kmitl.ac.th>
2019-06-23 08:13:37 +0700

    use arrow func return

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,19 @@
 import React from "react";
+const date = new Date();
+const hours = date.getHours();
 
+const calTime = in_hours => {
+  if (in_hours < 12) {
+    return "morning";
+  } else if (hours >= 12 && hours < 17) {
+    return "afternoon";
+  } else {
+    return "night";
+  }
+};
 const Footer = () => (
   <footer>
-    <h3>this is my footer </h3>
+    <h3>this is my footer Good {calTime(hours)} </h3>
   </footer>
 );
 export default Footer;
