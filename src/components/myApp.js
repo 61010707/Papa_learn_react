e606d4483692439647c9d9b8e9ec53f9f6b6eaea
DrZin <61010707@kmitl.ac.th>
2019-06-23 21:34:21 +0700

    can change state in todo

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -8,13 +8,27 @@ class MyApp extends React.Component {
     this.state = {
       todos: ToDoData
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleChange(id) {
+    this.setState(prevState => {
+      const updateTodo = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updateTodo
+      };
+    });
+  }
   render() {
     const ToDoComponents = this.state.todos.map(data => (
-      <ToDo key={data.id} datas={data} />
+      <ToDo key={data.id} datas={data} handleChange={this.handleChange} />
     ));
-    return <div>{ToDoComponents}</div>;
+    return <div className="todo-lists">{ToDoComponents}</div>;
   }
 }
 export default MyApp;
--- src/components/toDo.js
+++ src/components/toDo.js
@@ -1,27 +1,15 @@
 import React from "react";
 
-class ToDo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      completed: props.datas.completed,
-      text: props.datas.text
-    };
-  }
-
-  render() {
-    return (
-      <div className="todo-items">
-        <input
-          type="checkbox"
-          checked={this.state.completed}
-          onChange={() => {
-            console.log("click me !");
-          }}
-        />
-        <p>{this.state.text}</p>
-      </div>
-    );
-  }
-}
+const ToDo = props => {
+  return (
+    <div className="todo-items">
+      <input
+        type="checkbox"
+        checked={props.datas.completed}
+        onChange={() => props.handleChange(props.datas.id)}
+      />
+      <p>{props.datas.text}</p>
+    </div>
+  );
+};
 export default ToDo;
