9dfd2ddca899aa405ead2580bfde665a36caac30
DrZin <61010707@kmitl.ac.th>
2019-06-24 20:44:31 +0700

    reset code

--- src/components/myApp.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import FormsContainer from "./formsContainer";
-
-function myApp(params) {
-  return <FormsContainer />;
-}
-export default myApp;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import MyApp from "./components/myApp";
-
-ReactDOM.render(<MyApp />, document.getElementById("root"));
