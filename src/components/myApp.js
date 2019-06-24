c840976e3a8f74d9d656ff20677a40a962107daa
DrZin <61010707@kmitl.ac.th>
2019-06-24 18:37:53 +0700

    reset learn react form

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -3,33 +3,12 @@ import React, { Component } from "react";
 class myApp extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loading: false,
-      character: {}
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      loading: true
-    });
-    fetch("https://swapi.co/api/people/1")
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState({
-          loading: false,
-          character: data
-        });
-      });
+    this.state = {};
   }
   render() {
-    const text = this.state.loading
-      ? "loading..."
-      : this.state.character.name;
     return (
       <div>
-        <h1>{text}</h1>
+        <form>Hello inform</form>
       </div>
     );
   }
