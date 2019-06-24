fa328468e64ec4fa2aa6c2fa6b946bb9bf64ca00
DrZin <61010707@kmitl.ac.th>
2019-06-24 18:46:19 +0700

    use form in react > event pass value

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -3,12 +3,41 @@ import React, { Component } from "react";
 class myApp extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      firstName: "",
+      lastName: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+    console.log(this.state.firstName);
   }
   render() {
     return (
       <div>
-        <form>Hello inform</form>
+        <form>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First name "
+            onChange={this.handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last name"
+            onChange={this.handleChange}
+          />
+          <h2>
+            {this.state.firstName} {this.state.lastName}
+          </h2>
+        </form>
       </div>
     );
   }
