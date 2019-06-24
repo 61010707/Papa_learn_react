bf85419a8ae5d4aa12097289c316b91063eb5e9c
DrZin <61010707@kmitl.ac.th>
2019-06-24 19:24:48 +0700

    use form handle subnit

--- src/components/myApp.js
+++ src/components/myApp.js
@@ -6,24 +6,25 @@ class myApp extends Component {
     this.state = {
       firstName: "",
       lastName: "",
-      isFriendly: false
+      isFriendly: false,
+      gender: "",
+      favLang: ""
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
     const { name, value, type, checked } = event.target;
-    this.setState({
-      [name]: value
-    });
     type === "checkbox"
       ? this.setState({ [name]: checked })
       : this.setState({ [name]: value });
+    console.log(checked);
   }
+  handleSubmit() {}
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             name="firstName"
@@ -50,12 +51,47 @@ class myApp extends Component {
             />{" "}
             isFriendly ??
           </label>
-
-          <h2>
-            {this.state.firstName} {this.state.lastName}{" "}
-            {this.state.isFriendly ? "isFriendly" : null}
-            {}
-          </h2>
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={this.state.gender === "male"}
+              onChange={this.handleChange}
+            />{" "}
+            Male
+            <br />
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={this.state.gender === "female"}
+              onChange={this.handleChange}
+            />
+            Female
+          </label>
+          <br />
+          <label>favorite hello lang : </label>
+          <select
+            name="favLang"
+            value={this.state.favLang}
+            onChange={this.handleChange}
+          >
+            <option value="" />
+            <option value="hello ce">hello ce </option>
+            <option value="hello go">hello go</option>
+            <option value="hello c">hello c</option>
+            <option value="hello java">hello java </option>
+          </select>
+          <hr />
+          <h1>
+            My name is : {this.state.firstName} {this.state.lastName}
+          </h1>
+          <h2>He is : {this.state.isFriendly ? " friendly" : "so so"}</h2>
+          <h2> gender is {this.state.gender}</h2>
+          <h2>favorite hello lang is {this.state.favLang}</h2>
+          <button >submit !!!</button>
         </form>
       </div>
     );
