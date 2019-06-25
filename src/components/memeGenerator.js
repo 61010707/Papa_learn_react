f16652a27710a1519b57955f16c4b39510b19f97
DrZin <61010707@kmitl.ac.th>
2019-06-25 13:26:15 +0700

    add base state

--- src/components/memeGenerator.js
+++ src/components/memeGenerator.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+/**
+ * Initialize state to save the following data:
+ *      top text
+ *      bottom text
+ *      random image (intialize with "http://i.imgflip.com/1bij.jpg")
+ */
 
 class MemeGenerator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    render() {
-        return (
-            <h1>Meme Generrator</h1>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomTmg: "http://i.imgflip.com/1bij.jpg"
+    };
+  }
+  render() {
+    return <h1>Meme Generrator</h1>;
+  }
 }
 
 export default MemeGenerator;
