8dbcee2dc76217c41107d7acb0c7ac6e7a0c36d9
DrZin <61010707@kmitl.ac.th>
2019-06-25 13:22:44 +0700

    header and add css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MyApp from "./components/myApp";
-
+import "../src/index.css"
 ReactDOM.render(<MyApp />, document.getElementById("root"));
