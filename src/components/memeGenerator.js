f62ac52d28fd1a903023b2625726cbe2c99ff7ee
DrZin <61010707@kmitl.ac.th>
2019-06-25 14:16:26 +0700

    add submit handle

--- src/components/memeGenerator.js
+++ src/components/memeGenerator.js
@@ -15,6 +15,8 @@ class MemeGenerator extends Component {
       randomTmg: "http://i.imgflip.com/1bij.jpg",
       allMemeImgs: []
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   /**
    * We'll be using an API that provides a bunch of meme images.
@@ -27,13 +29,11 @@ class MemeGenerator extends Component {
   componentDidMount() {
     fetch("https://api.imgflip.com/get_memes")
       .then(response => response.json())
-
       .then(response => {
         const { memes } = response.data;
         console.log(memes[0]);
-        this.setState({ allMemeImg: memes });
+        this.setState({ allMemeImgs: memes });
       });
-    this.handleChange = this.handleChange.bind(this);
   }
 
   /**
@@ -44,10 +44,27 @@ class MemeGenerator extends Component {
     const { name, value } = event.target;
     this.setState({ [name]: value });
   }
+
+  /**
+   * Create a method that, when the "Gen" button is clicked, chooses one of the
+   * memes from our `allMemeImgs` array at random and makes it so that is the
+   * meme image that shows up in the bottom portion of our meme generator site
+   */
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // get a random int (index in the array)
+    const randNum = Math.floor(Math.random()* this.state.allMemeImgs.length) ;
+
+    // get the meme from that index
+    const readMemeImg = this.state.allMemeImgs[randNum].url;
+    // set `randomImg` to the `.url` of the random item I grabbed
+    this.setState({ randomTmg: readMemeImg });
+  }
   render() {
     return (
       <div>
-        <form className="meme-form">
+        <form className="meme-form" onSubmit={this.handleSubmit}>
           {/** * Create 2 input fields, one for the topText and one for the
           bottomText * Remember that these will be "controlled forms", so make
           sure to add * all the attributes you'll need for that to work */}
@@ -65,11 +82,14 @@ class MemeGenerator extends Component {
             value={this.state.bottomText}
             onChange={this.handleChange}
           />
+          <button>Gen</button>
         </form>
         <hr />
-        <h2>
-          {this.state.topText} {this.state.bottomText}
-        </h2>
+        <div className="meme">
+          <img src={this.state.randomTmg} alt="" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
       </div>
     );
   }
